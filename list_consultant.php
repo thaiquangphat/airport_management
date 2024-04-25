0fc83667cd7628d5b9a6876d955cd81840222984
thaiquangphat <quangphatthai@gmail.com>
2024-04-25 16:18:22 +0700

    fix list consultant at last else

--- list_consultant.php
+++ list_consultant.php
@@ -105,10 +105,11 @@ function delete_consultant($consultantid) {
             //         location.replace('index.php?page=list_consultant')
             //     }, 750)
             // }
+            //else {
+            //    alert_toast('Error: ' + resp,
+            //        "error"); // Display the error message returned from the server
+            //} 
             else {
-                alert_toast('Error: ' + resp,
-                    "error"); // Display the error message returned from the server
-            } else {
                 alert_toast('Data failed to delete.', "error");
                 setTimeout(function() {
                     location.reload();
